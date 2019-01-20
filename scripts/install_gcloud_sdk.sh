824d5274f97a09e571874ec62df46d712a4d63f5
Benjamin Sattler <bsattler.inbox@gmail.com>
2019-01-20 21:51:20 +0100

    Add gce login secret

--- scripts/install_gcloud_sdk.sh
+++ scripts/install_gcloud_sdk.sh
@@ -2,10 +2,11 @@
 
 set -ev
 curl https://sdk.cloud.google.com | bash > /dev/null
-source $HOME/google-cloud-sdk/path.bash.inc
+source ${HOME}/google-cloud-sdk/path.bash.inc
 
 gcloud --quiet components list
 gcloud --quiet components update
 gcloud --quiet components install beta kubectl
+gcloud --quiet auth activate-service-account --key-file ./k8s/secrets/gce.json
 gcloud --quiet --project ${K8S_PROJECT_NAME} container clusters get-credentials ${K8S_CLUSTER_NAME} --zone ${K8S_CLUSTER_ZONE}
 gcloud --quiet version
